55b649325b2a9891ad7a3b43714b62177628b36b
Lucas Gabriel Salina D'Ovidio <lugas@jfmedier.dk>
2020-02-28 13:12:27 +0100

    :sparkles: Add update item mutation and retrieve item query + resolvers and frontend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,6 +9,20 @@ const Mutations = {
         }, info);
 
         return item;
+    },
+
+    updateItem(parent, args, ctx, info) {
+        // take a copy of updates
+        const updates = { ...args }
+        // remove the ID from the updates
+        delete updates.id
+        // run the update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        }, info)
     }
 
 };
