0b736d05d4d2e7d84c6e9bed17412dd7a60866b2
Lucas Gabriel Salina D'Ovidio <lugas@jfmedier.dk>
2020-03-01 13:43:08 +0100

    :wrench: Auto prettier + lint

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,43 +1,51 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        // TODO check if they are logged in
+  async createItem(parent, args, ctx, info) {
+    // TODO check if they are logged in
 
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                ...args
-            }
-        }, info);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-        return item;
-    },
+    return item;
+  },
 
-    updateItem(parent, args, ctx, info) {
-        // take a copy of updates
-        const updates = { ...args }
-        // remove the ID from the updates
-        delete updates.id
-        // run the update method
-        return ctx.db.mutation.updateItem({
-            data: updates,
-            where: {
-                id: args.id
-            }
-        }, info)
-    },
+  updateItem(parent, args, ctx, info) {
+    // take a copy of updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
 
-    async deleteItem(parent, args, ctx, info) {
-        const where = {id: args.id}
-        // 1. find hte item
-        const item = await ctx.db.query.item({ where }, `{
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find hte item
+    const item = await ctx.db.query.item(
+      { where },
+      `{
             id
             title
-        }`)
-        // 2. check if they own the item or have the permissions
-        // TODO
-        // 3. delete it
-        return ctx.db.mutation.deleteItem({ where }, info)
-    }
-
+        }`
+    );
+    // 2. check if they own the item or have the permissions
+    // TODO
+    // 3. delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
