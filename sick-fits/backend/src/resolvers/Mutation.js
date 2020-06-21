c038681904f8af77d345dc241010238700429f6a
Lucas Gabriel Salina D'Ovidio <lugas@jfmedier.dk>
2020-06-21 21:58:43 +0200

    :sparkles: Add data relationship to Item model
    Scope: Lesson 32

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -6,11 +6,19 @@ const { transport, makeANiceEmail } = require("../mail");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in to create an item");
+    }
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          // this is how we provide relationships
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
           ...args
         }
       },
