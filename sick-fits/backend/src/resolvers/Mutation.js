dfaa65094bcce15b63754d87c3344d04b282b0bd
Lucas Gabriel Salina D'Ovidio <lugas@jfmedier.dk>
2020-06-21 14:50:06 +0200

    :sparkles: Sending emails
    Scope: Lesson 31

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { randomBytes } = require("crypto");
 const { promisify } = require("util");
+const { transport, makeANiceEmail } = require("../mail");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -128,8 +129,19 @@ const Mutations = {
       where: { email: args.email },
       data: { resetToken, resetTokenExpirty }
     });
-    return { message: "Thanks!" };
     // 3. Email them a reset token
+    const mailRes = await transport.sendMail({
+      from: "lucas@lucsali.com",
+      to: user.email,
+      subject: "Your password reset",
+      html: makeANiceEmail(`Your password reset token is here!
+      \n\n
+      <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">
+        Click here to reset
+      </a>`)
+    });
+    // 4.return the message
+    return { message: "Thanks!" };
   },
 
   async resetPassword(parent, args, ctx, info) {
