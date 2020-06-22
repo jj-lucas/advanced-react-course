981c7dc367b19ef571d80b10d6d396f615107fdf
Lucas Gabriel Salina D'Ovidio <lugas@jfmedier.dk>
2020-06-22 20:43:56 +0200

    :sparkles: Apollo local state queries and mutations
    Scope: Lesson 39

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,19 +1,43 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import withApollo from 'next-with-apollo'
+import ApolloClient from 'apollo-boost'
+import { endpoint } from '../config'
+import { LOCAL_STATE_QUERY } from '../components/Cart'
 
 function createClient({ headers }) {
-  return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: operation => {
-      operation.setContext({
-        fetchOptions: {
-          credentials: 'include',
-        },
-        headers,
-      });
-    },
-  });
+	return new ApolloClient({
+		uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+		request: operation => {
+			operation.setContext({
+				fetchOptions: {
+					credentials: 'include',
+				},
+				headers,
+			})
+		},
+		// local data
+		clientState: {
+			resolvers: {
+				Mutation: {
+					// destructure the cache from the client
+					toggleCart(_, variables, { cache }) {
+						// read the carOpen value from the cache
+						const { cartOpen } = cache.readQuery({
+							query: LOCAL_STATE_QUERY,
+						})
+						// write the cart state to the opposite
+						const data = {
+							data: { cartOpen: !cartOpen },
+						}
+						cache.writeData(data)
+						return data
+					},
+				},
+			},
+			defaults: {
+				cartOpen: true,
+			},
+		},
+	})
 }
 
-export default withApollo(createClient);
+export default withApollo(createClient)
