fb6cdf5bd6c3f4fbd52b58b8bff2d4916eb1a8f2
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-06-28 14:38:36 +0200

    :sparkles: Animating cart component
    Scope: Lesson 44

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,6 +4,7 @@ import User from './User'
 import Signout from './Signout'
 import { Mutation } from 'react-apollo'
 import { TOGGLE_CART_MUTATION } from './Cart'
+import CartCount from './CartCount'
 
 const Nav = () => (
 	<User>
@@ -25,7 +26,12 @@ const Nav = () => (
 						</Link>
 						<Signout />
 						<Mutation mutation={TOGGLE_CART_MUTATION}>
-							{toggleCart => <button onClick={toggleCart}>My cart</button>}
+							{toggleCart => (
+								<button onClick={toggleCart}>
+									My cart
+									<CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount>
+								</button>
+							)}
 						</Mutation>
 					</>
 				)}
