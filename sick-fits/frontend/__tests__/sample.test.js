9976e65c4480ae0daa8c9055c37bb656f9420e76
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-06-30 20:25:48 +0200

    :white_check_mark: Unit testing 101
    Scope: Lesson 55

---
+++ sick-fits/frontend/__tests__/sample.test.js
@@ -0,0 +1,9 @@
+describe('sample test 101', () => {
+	console.log('I am running some test')
+	it('works as expected', () => {
+		expect(1).toEqual(1)
+	})
+	fit('works as expected', () => {
+		expect(1).toEqual(1)
+	})
+})
