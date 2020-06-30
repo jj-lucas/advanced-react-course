53aaad6bde7ebe58797abda7ca103df4035e452c
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-06-30 20:53:33 +0200

    :white_check_mark: Mocking 101
    Scope: Lesson 56

---
+++ sick-fits/frontend/__tests__/item.js
@@ -0,0 +1,33 @@
+import ItemComponent from '../components/Item'
+import { shallow } from 'enzyme'
+import { ExpandAbstractTypes } from 'graphql-tools'
+import { isExists } from 'date-fns'
+
+const fakeItem = {
+	id: 'ABC123',
+	title: 'A Cool Item',
+	price: 5000,
+	description: 'This item is really cool!',
+	image: 'dog.jpg',
+	largeImage: 'largedog.jpg',
+}
+
+describe('<Item/>', () => {
+	fit('renders and displays properly', () => {
+		const wrapper = shallow(<ItemComponent item={fakeItem} />)
+		const PriceTag = wrapper.find('PriceTag')
+		expect(PriceTag.children().text()).toBe('$50')
+		// console.log(wrapper.debug())
+		expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
+		const img = wrapper.find('img')
+		expect(img.props().src).toBe(fakeItem.image)
+		expect(img.props().alt).toBe(fakeItem.title)
+
+		const buttonList = wrapper.find('.buttonList')
+		expect(buttonList.children()).toHaveLength(3)
+
+		expect(buttonList.find('Link')).toHaveLength(1)
+		expect(buttonList.find('Link').exists()).toBe(true)
+		expect(buttonList.find('Link')).toBeTruthy
+	})
+})
